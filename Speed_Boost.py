3e5d5f90c86c35dc42d40ca38859981b652e43d3
Mohd Shadab <shdb0011@gmail.com>
2021-02-25 22:00:06 +0530

    Revert "Output"
    This reverts commit 3c9e2c33d56b62150555d009434078f30be6149a.

--- Speed_Boost.py
+++ Speed_Boost.py
@@ -2,19 +2,10 @@ import shutil
 import os
 from tkinter import *
 
-def output():
-    #print("In output")
-    labelTitle.destroy()
-    labelName.destroy()
-    btnboost.destroy()
-    inputtxt.destroy()
-    labelOutput = Label(frame, text="Memory released!!\n\t\tSpeed is up.", font = ('Helvetica', 17, 'bold'), 
-                        bg="ghost white",fg="#74A059", anchor='e')
-    labelOutput.pack(fill='both')
-    labelOutput.place(x=1, y=50)
 
 def action():
     user = inputtxt.get("1.0", "end-1c")
+<<<<<<< HEAD
 <<<<<<< HEAD
     #print("C:\\Users\\" + user + "\\AppData\\Local\\Temp")
 ||||||| 162bd8c
@@ -23,6 +14,12 @@ def action():
 =======
     #print("In action")
 >>>>>>> alter
+||||||| 3c9e2c3 (Output)
+    #print("In action")
+=======
+    print("C:\\Users\\" + user + "\\AppData\\Local\\Temp")
+    print("In action")
+>>>>>>> parent of 3c9e2c3 (Output)
 
     paths = ["C:\\Windows\\prefetch", "C:\\Windows\\Temp", "C:\\Users\\" + user + "\\AppData\\Local\\Temp"]
 
@@ -46,10 +43,10 @@ root.title("PC Speed Booster")
 canvas = Canvas(root, height=200, width=400, bg="#0D1F2D")
 canvas.pack(fill = BOTH, expand = True)
 
-frame = Frame(root, bg="ghost white")
+frame = Frame(root, bg="white")
 frame.place(relheight=0.9, relwidth=0.9, relx=0.1, rely=0.2)
 labelTitle = Label(frame, text="Speed up your computer with a click", font = ('Helvetica', 15, 'bold'), height= 2,
-                bg="ghost white", fg="#74A059")
+                bg="ghostWhite", fg="#74A059")
 labelTitle.pack()
 labelTitle.place(x=5, y=0, rely=0.1)
 
@@ -59,18 +56,24 @@ inputtxt.pack()
 inputtxt.place(x=150, y=69)
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 labelName = Label(frame, text="User Name", font = ('Helvetica', 12, 'bold'), height= 2,
 ||||||| 162bd8c
 labelName = Label(frame, text="PC Name", font = ('Helvetica', 12, 'bold'), height= 2,
 =======
 labelName = Label(frame, text="User Name", font = ('Helvetica', 12, 'bold'), height= 1,
 >>>>>>> alter
+||||||| 3c9e2c3 (Output)
+labelName = Label(frame, text="PC Name", font = ('Helvetica', 12, 'bold'), height= 1,
+=======
+labelName = Label(frame, text="PC Name", font = ('Helvetica', 12, 'bold'), height= 2,
+>>>>>>> parent of 3c9e2c3 (Output)
                     bg="ghostWhite", fg="#435868")
 labelName.pack()
 labelName.place(x=50, y=69)
 
 btnboost = Button(frame, text="Boost", font = ('Helvetica', 9, 'bold'), padx=10, pady=5, fg="white", bg="#0D1F2D", 
-                command=lambda:[action(), output()], activeforeground = "red")
+                command=action, activeforeground = "red")
 btnboost.pack()
 btnboost.place(x=180, y=140, anchor=CENTER)
 
