d510dc78e25d05f4392e987ded1560529689242f
Mohd Shadab <shdb0011@gmail.com>
2021-02-25 11:29:33 +0530

    text field and color updation

--- Speed_Boost.py
+++ Speed_Boost.py
@@ -22,17 +22,28 @@ def action():
                     pass
 
 
-canvas = Canvas(root, height=200, width=400, bg="#263D42")
+canvas = Canvas(root, height=200, width=400, bg="#0D1F2D")
 canvas.pack(fill = BOTH, expand = True)
 
 frame = Frame(root, bg="white")
 frame.place(relheight=0.9, relwidth=0.9, relx=0.1, rely=0.2)
-label = Label(frame, text="Speed up your computer with a click", font = ('Helvetica', 15, 'bold'), height= 2, bg="ghostWhite", fg="red")
-label.pack()
-label.place(relx = 0.0, rely=0.1)
+labelTitle = Label(frame, text="Speed up your computer with a click", font = ('Helvetica', 15, 'bold'), height= 2,
+                bg="ghostWhite", fg="#74A059")
+labelTitle.pack()
+labelTitle.place(x=5, y=0, rely=0.1)
 
-boost = Button(frame, text="Boost", font = ('Helvetica', 9, 'bold'), padx=10, pady=5, fg="white", bg="#263D42", command=action, activeforeground = "red")
-boost.pack()
-boost.place(relx=0.5, rely= 0.55, anchor=CENTER)
+btnboost = Button(frame, text="Boost", font = ('Helvetica', 9, 'bold'), padx=10, pady=5, fg="white", bg="#0D1F2D", 
+                command=action, activeforeground = "red")
+btnboost.pack()
+btnboost.place(x=180, y=140, anchor=CENTER)
 
+inputtxt = Text(frame, height = 1.8,
+                width = 20, bg = "light grey")
+inputtxt.pack()
+inputtxt.place(x=150, y=69)
+
+labelName = Label(frame, text="PC Name", font = ('Helvetica', 12, 'bold'), height= 2,
+                    bg="ghostWhite", fg="#435868")
+labelName.pack()
+labelName.place(x=50, y=69)
 root.mainloop()
